a86ee4b325a896ec6260e1420e90bbbce60d2b07
Chandrasekhar Gunnam <chandrasekhar.gunnam@Aveva.com>
2018-03-22 15:04:40 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,29 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { MaterialModule } from './material.module';
+import { MatLoginComponent } from './mat-login/mat-login.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+{path: '', redirectTo: '/login', pathMatch: 'full'},
+{path: 'login', component: MatLoginComponent  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatLoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
